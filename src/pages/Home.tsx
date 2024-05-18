8af5ff07aa99b16e6a4f1b2cdcdd6b08e721ab53
Serhii Shabala <sergei181989@gmail.com>
2024-05-18 20:25:55 +0300

    :recycle: Refactor Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,13 @@ import { Box } from 'components/common';
 export const Home = () => {
   return (
     <Box task>
-      <h1>Home page</h1>
+      <h1 className='text-3xl text-slate-300 text-center mb-3'>
+        Learn ReactJS and other libraries.
+      </h1>
+      <p className='text-lg'>
+        On the left, you can click on different challenges that were created by
+        ChatGPT to help me practice and learn ReactJS 👀
+      </p>
     </Box>
   );
 };
